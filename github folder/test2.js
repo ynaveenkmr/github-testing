ef42af7e79b4c8493ffda91e7b3d81a46de175ca
naveen <ynaveenkmr@gmail.com>
2021-07-13 17:31:21 +0530

    another function add

--- github folder/test2.js
+++ github folder/test2.js
@@ -7,6 +7,16 @@ var ot = testAdd(add,5);
 console.log(ot); 
 
 /**Another function example */
+function display(some){
+console.log(some);
+}
+
+function testAdd(n1,n2,callback){
+var output=n1+n2;
+callback(output);
+}
+ testAdd(5,5,display);
+ 
  /**Take update one */
 
 alert('ok');
