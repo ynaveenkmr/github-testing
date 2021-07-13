80b8d2ec3efda6f54d81f289009d0c76dfcefc94
naveen <ynaveenkmr@gmail.com>
2021-07-13 19:42:24 +0530

    switch statement updated

--- github folder/test2.js
+++ github folder/test2.js
@@ -17,6 +17,24 @@ var output=n1+n2;
 callback(output);
 }
  testAdd(5,5,display);
+
+ /**simple switch statement */
+
+ var a = 5;
+
+switch (a) {
+  case 3:
+    alert( 'Too small' );
+    break;
+  case 4:
+    alert( 'Exactly!' );
+    break;
+  case 5:
+    alert( 'Too big' );
+    break;
+  default:
+    alert( "I don't know such values" );
+}
  
 
   </script>
